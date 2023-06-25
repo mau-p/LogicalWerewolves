36f86cf34bd07188f3b0d9c3a64ea07d2a8251fa
MH-v <maartenheiko@gmail.com>
2023-06-25 02:28:44 +0200

    Script should be adjusted to add higher order

--- main_experiment.py
+++ main_experiment.py
@@ -5,21 +5,23 @@ from tqdm import tqdm
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    use_2nd_order = [False]
     total_agents = 20
     args = parser.parse_args()
     n_games = 500
-    stats = pd.DataFrame(columns=['knowledge_order', 'n_werewolves', 'winrate', 'correct_score', 'num_rounds'])
+    stats = pd.DataFrame(columns=['higher_order', 'n_werewolves', 'winrate', 'correct_score', 'num_rounds'])
+    stats['higher_order'] = stats['higher_order'].astype(bool)
 
-    for n_werewolves in range(1, 5):
-        for knowledge_order in range(1, 11):
+    for higher_order in tqdm(use_2nd_order):
+        for n_werewolves in range(1, total_agents//2):
             winrate = 0
             correct_score = 0
             num_rounds = 0
             for _ in range(n_games):
-                game_instance = game.Game(knowledge_order=knowledge_order, number_werewolves=n_werewolves, number_villagers=total_agents-n_werewolves-1)
+                game_instance = game.Game(number_werewolves=n_werewolves, number_villagers=total_agents-n_werewolves-1, higher_order=higher_order)
                 winrate_single, correct_score_single, num_rounds_single = game_instance.run_game()
                 winrate += winrate_single
                 correct_score += correct_score_single
                 num_rounds += num_rounds_single
-            stats = pd.concat([stats, pd.DataFrame({'knowledge_order': [knowledge_order], 'n_werewolves': [n_werewolves], 'winrate': [winrate/n_games], 'correct_score': [correct_score/n_games], 'num_rounds': [num_rounds/n_games]})], ignore_index=True)
-    stats.to_csv('stats.csv')   
+            stats = pd.concat([stats, pd.DataFrame({'higher_order': [higher_order], 'n_werewolves': [n_werewolves], 'winrate': [winrate/n_games], 'correct_score': [correct_score/n_games], 'num_rounds': [num_rounds/n_games]})], ignore_index=True)
+    stats.to_csv('stats.csv', index=False)
